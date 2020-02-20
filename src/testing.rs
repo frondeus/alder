870e79bafd7b687b5054feb7cf2ea243bb07d1f8
Wojciech Polak <wojciech.polak@anixe.pl>
2020-02-20 11:28:12 +0100

    Fix on Windows

--- src/testing.rs
+++ src/testing.rs
@@ -2,19 +2,22 @@ pub fn snap(actual_debug: String, file: &str, module: &str, test_case_name: &str
     println!("{}", actual_debug);
 
     use std::io::Write;
+    let module = module.replace("::", "_");
     let mut dir_path = std::path::PathBuf::from(file);
 
     dir_path = dir_path.parent().expect("Parent directory").into();
     dir_path.push("snaps");
-
     let dir_path_str = dir_path.to_str().unwrap();
+
     let path: std::path::PathBuf =
-        format!("{}/{}::{}.snap", dir_path_str, &module, test_case_name).into();
+        format!("{}/{}_{}.snap", dir_path_str, &module, test_case_name).into();
     let new_path: std::path::PathBuf =
-        format!("{}/{}::{}.snap.new", dir_path_str, &module, test_case_name).into();
+        format!("{}/{}_{}.snap.new", dir_path_str, &module, test_case_name).into();
 
     if !path.exists() {
+        dbg!(&dir_path);
         let _r = std::fs::create_dir_all(&dir_path);
+        dbg!(&new_path);
         std::fs::File::create(&new_path)
             .and_then(|mut file| file.write_all(actual_debug.as_bytes()))
             .expect("Couldn't save snap");
